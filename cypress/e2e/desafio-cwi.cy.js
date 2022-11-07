fdeecd47b3eb65bd61d7a62a0e5b1d2af7b50bc3
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-11-07 18:38:12 -0300

    Desafio parcialmente concluido:   Acessar Página: OK   Cadastrar Usuário: OK   Adicionar Endereço: PENDENTE   Encerrar em My Account: PENDENTE
    Changes to be committed:
    	modified:   cypress.config.js
    	modified:   cypress/e2e/desafio-cwi.cy.js

--- cypress/e2e/desafio-cwi.cy.js
+++ cypress/e2e/desafio-cwi.cy.js
@@ -1,7 +1,31 @@
+/// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
+
 describe("Desafio Automação Web - CWI", () => {
   beforeEach(() => {
-    cy.visit("https://magento.softwaretestingboard.com");
+    cy.visit("/");
   });
 
-  it("Deve cadastrar uma conta de usuário", () => {});
+  it("Deve cadastrar uma conta de usuário", () => {
+    cy.visit("/customer/account/create/");
+
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
+    let email = faker.internet.email(firstName);
+    let password = "pDdM<$$2331@";
+
+    // pageAccountCreate - PersonalInformation
+    cy.get("#firstname").type(firstName);
+    cy.get("#lastname").type(lastName);
+
+    // pageAccountCreate - SignInInformation
+    cy.get("#email_address").type(email);
+    cy.get("#password").type(password);
+    cy.get("#password-confirmation").type(password);
+
+    // pageAccountCreate - Confirmation
+    cy.get("button[title='Create an Account']").click();
+
+    cy.get(".message-success").should("exist");
+  });
 });
