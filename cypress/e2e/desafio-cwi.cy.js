b5816919bd8ba32b1679acf7cd97b4190c6636c7
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-11-07 17:24:39 -0300

    first commit

---
+++ cypress/e2e/desafio-cwi.cy.js
@@ -0,0 +1,7 @@
+describe("Desafio Automação Web - CWI", () => {
+  beforeEach(() => {
+    cy.visit("https://magento.softwaretestingboard.com");
+  });
+
+  it("Deve cadastrar uma conta de usuário", () => {});
+});
