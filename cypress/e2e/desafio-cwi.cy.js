14f695cf5ca257bb756502c0c49c9278a0abf063
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-11-07 19:37:04 -0300

    Desafio concluido:   Acessar Página: OK   Cadastrar Usuário: OK   Adicionar Endereço: OK   Encerrar em My Account: OK
    Melhorias:
      Utilizar Page Objects
    
    Changes to be committed:
    	modified:   cypress/e2e/desafio-cwi.cy.js

--- cypress/e2e/desafio-cwi.cy.js
+++ cypress/e2e/desafio-cwi.cy.js
@@ -13,6 +13,11 @@ describe("Desafio Automação Web - CWI", () => {
     let lastName = faker.name.lastName();
     let email = faker.internet.email(firstName);
     let password = "pDdM<$$2331@";
+    let company = faker.company.name();
+    let address1 = faker.address.streetAddress();
+    let city = faker.address.city();
+    let zipCode = faker.address.zipCode("#####");
+    let phone = faker.phone.number("904 ### ####");
 
     // pageAccountCreate - PersonalInformation
     cy.get("#firstname").type(firstName);
@@ -27,5 +32,33 @@ describe("Desafio Automação Web - CWI", () => {
     cy.get("button[title='Create an Account']").click();
 
     cy.get(".message-success").should("exist");
+    cy.get("[data-ui-id='page-title-wrapper']").should(
+      "contain.text",
+      "My Account"
+    );
+
+    // pageCustomerAccount - Address Book / Default Billing Address
+    cy.get("[data-ui-id='default-billing-edit-link']").click();
+
+    // pageCustomerAddress - Add New Address / Contact Information
+    cy.get("#firstname").clear().type(firstName);
+    cy.get("#lastname").clear().type(lastName);
+    cy.get("#company").type(company);
+    cy.get("#telephone").type(phone);
+
+    // pageCustomerAddress - Add New Address / Address
+    cy.get("#street_1").clear().type(address1);
+    cy.get("#city").clear().type(city);
+    cy.get("#region_id").select("18").should("have.value", "18");
+    cy.get("#zip").type(zipCode);
+    cy.get("#country").select("US").should("have.value", "US");
+
+    // pageCustomerAddress - Confirmation
+    cy.get("[data-action='save-address']").click();
+
+    cy.get("[data-ui-id='message-success']").should("exist");
+
+    // pageCustomerAccount - My Account
+    cy.get("#block-collapsible-nav li:nth-child(1)").click();
   });
 });
