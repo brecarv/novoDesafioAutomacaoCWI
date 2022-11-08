1bda1f0636d553ee3e55967c0d61443c07f4693e
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-11-08 19:53:09 -0300

    PageAccountCreation added

---
+++ cypress/pages/page-account-creation.js
@@ -0,0 +1,32 @@
+class PageAccountCreation {
+  url = "/customer/account/create";
+  inputFirstName = "#firstname";
+  inputLastName = "#lastname";
+  inputEmail = "#email_address";
+  inputPassword = "#password";
+  inputPasswordConfirmation = "#password-confirmation";
+  buttonCreateAnAccount = "button[title='Create an Account']";
+
+  visitPage() {
+    cy.visit(this.url);
+  }
+
+  fillPersonalInformation(firstName, lastName) {
+    cy.get(this.inputFirstName).type(firstName);
+    cy.get(this.inputLastName).type(lastName);
+  }
+
+  fillSignupInformation(email, password) {
+    cy.get(this.inputEmail).type(email);
+    cy.get(this.inputPassword).type(password);
+    cy.get(this.inputPasswordConfirmation).type(password);
+  }
+
+  createAccount(firstName, lastName, email, password) {
+    this.fillPersonalInformation(firstName, lastName);
+    this.fillSignupInformation(email, password);
+    cy.get(this.buttonCreateAnAccount).click();
+  }
+}
+
+export default PageAccountCreation;
